00328d989b81de8bbff0527d93000c05ddecb96a
BrantK <brant@cyberdust.com>
2016-03-28 17:06:06 -0700

    updated elements

--- CDAutomation/src/com/cyberdust/automation/elements/AndroidElements.java
+++ CDAutomation/src/com/cyberdust/automation/elements/AndroidElements.java
@@ -708,4 +708,55 @@ public class AndroidElements extends Drivers {
     public WebElement tutorial_close() {
     	return wait.until(ExpectedConditions.elementToBeClickable(By.id("com.radicalapps.cyberdust:id/tutorial_close_button")));
     }
+
+	/*************************
+	 * New Tutorial elements *
+	 *************************/
+	public WebElement tutorial_icon() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//android.widget.FrameLayout[@index='0'][android.widget.FrameLayout[@index='1'][android.widget.FrameLayout[@index='']")));
+	}
+
+	public WebElement tutorial_private_messaging() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Private messaging with Cyber Dust")));
+	}
+
+	public WebElement tutorial_private_networking() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Private networking with other Cyber Dusters")));
+	}
+
+	public WebElement tutorial_getStarted_Button() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Get Started!")));
+	}
+
+	public WebElement tutorial_username() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Get Pick a username")));
+	}
+
+	public WebElement tutorial_profile_photo() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Select a profile photo")));
+	}
+
+	public WebElement tutorial_find_contacts() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Find contacts with Cyber Dust")));
+	}
+
+	public WebElement tutorial_leart_to_dust() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Learn to Dust")));
+	}
+
+	public WebElement tutorial_build_a_following() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Build a following")));
+	}
+
+	public WebElement tutorial_facebook_friends() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Find friends from Facebook")));
+	}
+
+	public WebElement tutorial_share_profile() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Share your profile")));
+	}
+
+	public WebElement tutorial_learn_about_blasts() {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.name("Learn about Blasts")));
+	}
 }
