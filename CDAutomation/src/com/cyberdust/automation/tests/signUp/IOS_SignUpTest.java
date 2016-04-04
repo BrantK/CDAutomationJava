86874f4f61fec88634aaa39ad6e0a16edd0dd1f5
BrantK <brant@cyberdust.com>
2016-04-04 12:19:45 -0700

    updated to work with new elements

--- CDAutomation/src/com/cyberdust/automation/tests/signUp/IOS_SignUpTest.java
+++ CDAutomation/src/com/cyberdust/automation/tests/signUp/IOS_SignUpTest.java
@@ -3,9 +3,9 @@ package com.cyberdust.automation.tests.signUp;
 import com.cyberdust.automation.elements.AndroidCamera;
 import com.cyberdust.automation.elements.IOSElements;
 
+import javafx.scene.control.Alert;
+
 public class IOS_SignUpTest extends IOSElements {
-	
-	AndroidCamera androidCamera = new AndroidCamera();
 	
 	public void test01_check_logged_out() throws Exception {
 		// Check if logged out
@@ -31,65 +31,38 @@ public class IOS_SignUpTest extends IOSElements {
 	public void test02_sign_up() throws Exception {
 		// Create new account and check if special characters can be used
 	    sign_up_button().click();
-	    pick_username().sendKeys(signup_account + "!@//$");
+	    driver.getKeyboard().sendKeys(signup_account);
+        Thread.sleep(1200);
+        driver.getKeyboard().sendKeys("!@//$");
+
 	    try {
-	    	waitTime(3);
-	        sign_up_OK().click();
-	        log("Special characters used in username");
-	    } catch (Exception e) {
+	    	waitTime(2);
+	        username_OK().click();
+            if (password_OK().isDisplayed()) {
+                log("Special characters used in username");
+            }
+        } catch (Exception e) {
 	        log("Could not use special characters in username");
+            choose_username().clear();
+            driver.getKeyboard().sendKeys(signup_account);
+            username_OK().click();
 	    }
 	    waitTime(20);
-	    pick_username().sendKeys(signup_account);
-	    sign_up_OK().click();
-	    create_password().sendKeys(signup_password);
-	    confirm_password().sendKeys(signup_password);
-	    password_OK().click();
-	    birthday().click(); Thread.sleep(2000);
-	    		
-        // Sets birthday
-        log("Entering birthday");
-        action.longPress(date().getLocation().x, date().getLocation().y, 3000).release().perform();
-        birthday_done().click();
-        birthday_OK().click();
 
-        // Enters email
-        log("Entering email");
-        email().sendKeys(signup_email);
-        email_OK().click();
-        OK_button().click();
+	    driver.getKeyboard().sendKeys(signup_password);
+	    password_OK().click();
+	    birthday_OK().click(); Thread.sleep(2000);
 	}
 	
 	public void test03_take_profile_pic() throws Exception {
 		// Takes a picture with camera and sets as profile picture
-        sign_up_profile_pic().click();
-        camera_button().click(); Thread.sleep(3000);
-        log("Taking a photo and setting it as the profile picture");
-        androidCamera.takePhoto();
-        log("Skipping checking contacts and sending text message");
-        OK_button().click();
         skip_button().click();
-        try {
-        	waitTime(2);
-        	done_button().click();
-        } catch (Exception e) {
-        	skip_button().click();
-        	done_button().click();
-        }
-        log("New account created");
+        skip_button().click();
+        skip_button().click();
 	}
 	
 	public void test04_check_for_tutorial() throws Exception {
-		try {
-			waitTime(5);
-			if (tutorial_video().isDisplayed()) {
-				Thread.sleep(1000);
-				log("Backing out of tutorial screen");
-				//driver.pressKeyCode(4);
-			}
-		} catch (Exception e) {
-			log("No tutorial screen");
-		}
+		tutorial_close().click();
 	}
 	
     public void test05_update_profile_pic() throws Exception {
@@ -98,11 +71,20 @@ public class IOS_SignUpTest extends IOSElements {
         profile_picture().click();
         change_profile_picture().click();
         log("Changing profile picture");
-        camera_button().click(); Thread.sleep(3000);
-        androidCamera.takePhoto();
+        photo_button().click();
+
+        if (name("Export failed!").isDisplayed()) {
+            log("Using gallery photo");
+            name("OK").click();
+            photo_gallery().click();
+            camera_roll().click();
+            camera_roll_photo1().click();
+        }
+
         log("Profile picture updated");
-	}
-	
+        profile_picture_done().click();
+    }
+
     public void test06_login_logout() throws Exception {
         // Logout and login test
         log("Logging out then logging in");
@@ -110,16 +92,18 @@ public class IOS_SignUpTest extends IOSElements {
         logout().click();
         confirm().click();
         login_button().click();
-        //login_username().sendKeys(signup_account.toUpperCase());
-        login_password().sendKeys(signup_password);
+        login_username().click();
+        driver.getKeyboard().sendKeys(signup_account.toUpperCase() + "\n" + signup_password);
         login_OK().click();
         log("Username is not case sensitive");
 
         // Deletes account
         more_button().click(); Thread.sleep(1000);
         action.press(followers()).moveTo(back_button()).release().perform();
+        account_settings().click();
         delete_account().click();
+        confirm_delete().click();
         log("Deleting account");
-        confirm().click();
+        confirm_delete_again().click();
 	}
 }
