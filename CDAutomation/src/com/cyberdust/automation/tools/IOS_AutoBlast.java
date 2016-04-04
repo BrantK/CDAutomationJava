3319ffb7324de6afa3033d63e8c946775d3b9112
BrantK <brant@cyberdust.com>
2016-04-04 12:19:08 -0700

    fixed

--- CDAutomation/src/com/cyberdust/automation/tools/IOS_AutoBlast.java
+++ CDAutomation/src/com/cyberdust/automation/tools/IOS_AutoBlast.java
@@ -1,26 +1,26 @@
 package com.cyberdust.automation.tools;
 
 import com.cyberdust.automation.elements.IOSElements;
-import io.appium.java_client.TouchAction;
 import org.junit.*;
 
-
 public class IOS_AutoBlast extends IOSElements {
-	String blast_recipient = "bktest01";
-	String blast_type = "text";  // Use photo, video, text, or a combination of the 3
+	String blast_recipient = "bktest02";
+	String blast_type = "text, photo";  // Use photo, video, text, or a combination of the 3
 	String blast_text = "Test";  // Only works for text blasts
-	int number_of_blasts = 1;  // Number of blasts per type
-	
-	TouchAction action = new TouchAction(driver);
+	int number_of_blasts = 50;  // Number of blasts per type
 	
     public void photo_blast() throws Exception {
+        Thread.sleep(500);
         action_menu().click();
+        Thread.sleep(500);
         action_menu_media().click();
-        photo_button().click();
+        photo_gallery().click();
+        camera_roll().click();
+        camera_roll_photo1().click();
         next_button().click();
         blast_friends().click();
         username(blast_recipient).click();
-        blast_Ok_button().click();
+        action.press(blast_Ok_button().getLocation().getX(), blast_Ok_button().getLocation().getY()-10).release().perform();
     }
     public void video_blast() throws Exception {
         action_menu().click();
@@ -30,33 +30,37 @@ public class IOS_AutoBlast extends IOSElements {
         next_button().click();
         blast_friends().click();
         username(blast_recipient).click();
-        blast_Ok_button().click();
+        action.press(blast_Ok_button().getLocation().getX(), blast_Ok_button().getLocation().getY()-10).release().perform();
     }
     public void text_blast() throws Exception {
         action_menu().click();
         action_menu_text().click();
-        text_blast_field().sendKeys(blast_text);
-        OK_button().click();
+        driver.getKeyboard().sendKeys(blast_text);
+        next_button().click();
         blast_friends().click();
         username(blast_recipient).click();
-        blast_Ok_button().click();
+        action.press(blast_Ok_button().getLocation().getX(), blast_Ok_button().getLocation().getY()-10).release().perform();
     }
+
     @Test
     public void autoblast() throws Exception {
-    	for (int i = 0; i < number_of_blasts; i++) {
+    	for (int i = 0; i < number_of_blasts;) {
     		
     		if (blast_type.toLowerCase().contains("text".toLowerCase())) {
     			text_blast();
-    			System.out.println("Sending text blast: #" + (i+1));
+    			System.out.println("Sending blast: #" + (i+1));
+                i+=1;
     		}
     		if (blast_type.toLowerCase().contains("photo".toLowerCase())) {
     			photo_blast();
-    			System.out.println("Sending photo blast: #" + (i+1));
-    	}
+    			System.out.println("Sending blast: #" + (i+1));
+                i+=1;
+    	    }
     		if (blast_type.toLowerCase().contains("video".toLowerCase())) {
     			video_blast();
-    			System.out.println("Sending video blast: #" + (i+1));
-    	}
+    			System.out.println("Sending blast: #" + (i+1));
+                i+=1;
+    	    }
+        }
     }
 }
-    }
