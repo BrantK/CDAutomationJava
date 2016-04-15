54d0f10bc1f8376f4619d512a9aeaad6b56671f2
Ian R. Connelly <ian@cyberdust.com>
2016-04-15 16:24:23 -0700

    update: iOS Find Tab Tests
    Re-polished post-appium updates. Also some slight stability improvements.

--- CDAutomation/src/com/cyberdust/automation/tests/find/IOS_Find.java
+++ CDAutomation/src/com/cyberdust/automation/tests/find/IOS_Find.java
@@ -3,14 +3,20 @@ package com.cyberdust.automation.tests.find;
 import com.cyberdust.automation.elements.IOSElements;
 import com.cyberdust.automation.elements.LoginWith;
 
+import org.openqa.selenium.WebElement;
+
 public class IOS_Find extends IOSElements{
 
     LoginWith loginAs = new LoginWith();
 
     public void test01_followChatter() throws Exception {
 
-        loginAs.user(find_account01, find_password01);
-        System.out.println("Logged In");
+        try {
+            logoutAccount();
+        } catch (Exception ignored) {}
+
+        createAccount("findtester", "password");
+
         System.out.println("Following Account from chatter stream.");
         find_tab().click();
         first_chatter_add().click();
@@ -27,15 +33,10 @@ public class IOS_Find extends IOSElements{
 
     public void test02_follow_publisher() throws Exception {
 
-        //corrects screen state if previous test failed or app crashed
-        try {
-            if (empty_dust_tab_text().getAttribute("name").equals("You have no Dusts")) {
-                find_tab().click();
-            }
-        } catch (Exception e) {}
+        find_tab().click();
 
         System.out.println("Following Publisher from publisher stream.");
-        action.press(featured_people_banner()).moveTo(dusts_tab()).waitAction(1000).release().perform();
+
         first_publisher_add().click();
         first_publisher_open_profile().click();
         if ( !profile_follow_button().getAttribute("value").equals("1")) {
@@ -50,14 +51,13 @@ public class IOS_Find extends IOSElements{
 
     public void test03_category_titles() throws Exception {
 
-        //corrects screen state if previous test crashed or app crashed
-        try {
-            if (empty_dust_tab_text().getAttribute("name").equals("You have no Dusts")) {
-                find_tab().click();
-            }
-        } catch (Exception e) {}
-
         System.out.println("Testing chatter categories match descriptions");
+
+        try {
+            find_tab().click();
+            action.press(featured_people_banner()).moveTo(dusts_tab()).waitAction(1000).release().perform();
+        } catch (Exception ignored) {}
+
         action.press(categories_banner()).moveTo(dusts_tab()).waitAction(1000).release().perform();
 
         //////Advertising category//////
@@ -67,6 +67,28 @@ public class IOS_Find extends IOSElements{
         if (!first_chatter_category().getAttribute("name").equals("Advertising")) {
             throw new InterruptedException("Advertising chatter tag does not match category.");
         }
+
+        int counter = 0; //<-- Repeat attempts added for increased stability (it was a problem on some devices)
+        while (!first_chatter_category().getAttribute("name").equals("Advertising")) {
+            if (counter >= 3) {
+                throw new InterruptedException("Advertising chatter tag does not match category.");
+            }
+
+            previous_screen_button().click();
+            previous_screen_button().click();
+            Thread.sleep(1000);
+            try {
+                category_advertising().click();
+            } catch (Exception ignored) {
+                action.press(categories_banner()).moveTo(dusts_tab()).waitAction(1000).release().perform();
+                category_advertising().click();
+            }
+            Thread.sleep(500);
+            category_advertising_marketing().click();
+
+            counter++;
+        }
+
         System.out.println("Okay");
         previous_screen_button().click();
         previous_screen_button().click();
@@ -79,6 +101,27 @@ public class IOS_Find extends IOSElements{
         if (!first_chatter_category().getAttribute("name").equals("Arts")) {
             throw new InterruptedException("Dance chatter tag does not match category.");
         }
+
+        counter = 0;
+        while (!first_chatter_category().getAttribute("name").equals("Arts")) {
+            if (counter >= 3) {
+                throw new InterruptedException("Dance chatter tag does not match category.");
+            }
+            previous_screen_button().click();
+            previous_screen_button().click();
+            Thread.sleep(1000);
+            try {
+                category_arts().click();
+            } catch (Exception ignored) {
+                action.press(categories_banner()).moveTo(dusts_tab()).waitAction(1000).release().perform();
+                category_arts().click();
+            }
+            Thread.sleep(500);
+            category_arts_dancing().click();
+
+            counter++;
+        }
+
         System.out.println("Okay");
         previous_screen_button().click();
         previous_screen_button().click();
@@ -87,10 +130,39 @@ public class IOS_Find extends IOSElements{
         //////Business category//////
         System.out.print("Testing \"Business\"... ");
         category_business().click();
-        category_business_finance().click();
+
+        try {
+            category_business_finance().click();
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            action.press(categories_banner()).moveTo(business_nav_bar()).waitAction(1000).release().perform();
+            category_business_finance().click();
+        }
+
         if (!first_chatter_category().getAttribute("name").equals("Business")) {
             throw new InterruptedException("Business chatter tag does not match category.");
         }
+
+        counter = 0;
+        while (!first_chatter_category().getAttribute("name").equals("Business")) {
+            if (counter >= 3) {
+                throw new InterruptedException("Business chatter tag does not match category.");
+            }
+            previous_screen_button().click();
+            previous_screen_button().click();
+            Thread.sleep(1000);
+            try {
+                category_business().click();
+            } catch (Exception ignored) {
+                action.press(categories_banner()).moveTo(dusts_tab()).waitAction(1000).release().perform();
+                category_business().click();
+            }
+            Thread.sleep(500);
+            category_business_finance().click();
+
+            counter++;
+        }
+
         System.out.println("Okay");
         previous_screen_button().click();
         previous_screen_button().click();
@@ -99,50 +171,39 @@ public class IOS_Find extends IOSElements{
     public void test04_build_a_following() throws Exception {
 
         //corrects screen state if previous test crashed or app crashed
-        try {
-            if (empty_dust_tab_text().getAttribute("name").equals("You have no Dusts")) {
-                find_tab().click();
-            }
-        } catch (Exception e) {}
 
         try {
+            find_tab().click();
             action.press(category_advertising()).moveTo(temp_tutorial_placeholder()).release().perform();
         } catch (Exception e) {}
 
-
-        //Cleans up form from last test
-        System.out.print("Cleaning up fields from last test... ");
         build_a_following_button().click();
-        select_a_category_button().click();
-        previous_screen_button().click();
-        build_a_following_description().click();
-        build_a_following_description().click();
-        action.longPress(delete_button(), 3000).release().perform();
-        System.out.println("Okay");
 
+        BAF_form_send_button().click();
 
-        blast_Ok_button().perform();;
         if (!(error_message().getAttribute("name")).equals("Error")){
-            throw new InterruptedException("Submitted blank \"Build A Following\" form");
+            throw new InterruptedException("Submitted blank \"Build A Following\" form (no prof pic)");
+        }
+        OK_button().click();
+
+        build_a_following_description().click();
+        driver.getKeyboard().sendKeys("test");
+        BAF_form_send_button().click();
+        if (!(error_message().getAttribute("name")).equals("Error")){
+            throw new InterruptedException("\"Build A Following\" form submitted with description less than 6 characters (no prof pic)");
         }
         OK_button().click();
         build_a_following_description().click();
-        driver.getKeyboard().sendKeys("test\n");
-        if (!(error_message().getAttribute("name")).equals("Error")){
-            throw new InterruptedException("\"Build A Following\" form submitted with description less than 6 characters");
-        }
-        OK_button().click();
-        build_a_following_description().click();
-        driver.getKeyboard().sendKeys("tt");
+        driver.getKeyboard().sendKeys("test");
         build_following_lower_keyboard().click();
 
-        ////////Remember to add a test case for category selection and profile pic after bug is fixed. ///////
+        //TODO: Add a test case for category selection and profile pic after bug is fixed. ///////
         select_a_category_button().click();
         category_advertising().click();
         no_button().click();
 
-        blast_Ok_button().perform();
-        System.out.println("Form submitted successfully");
+        BAF_form_send_button().click();
+        System.out.println("Form submitted (no prof pic)");
         OK_button().click();
 
 
@@ -150,12 +211,7 @@ public class IOS_Find extends IOSElements{
 
     public void test05_searchbar() throws Exception {
 
-        //corrects screen state if previous test crashed or app crashed
-        try {
-            if (empty_dust_tab_text().getAttribute("name").equals("You have no Dusts")) {
-                find_tab().click();
-            }
-        } catch (Exception e) {}
+        find_tab().click();
 
         action.press(contact_banner()).moveTo(featured_people_banner()).release().perform();
         System.out.println("Trying to follow user in search results...");
@@ -174,5 +230,45 @@ public class IOS_Find extends IOSElements{
         close_profile().click();
         close_search().click();
 
+        deleteAccount();
+
     }
+
+    public void createAccount (String username, String password){
+        log("Creating account...");
+        sign_up_button().click();
+        driver.getKeyboard().sendKeys(username);
+        username_OK().click();
+        driver.getKeyboard().sendKeys(password);
+        password_OK().click();
+        birthday_OK().click();
+        skip_button().click();
+        skip_button().click();
+        skip_button().click();
+        tutorial_close().click();
+        log("Account created");
+    }
+
+    public void deleteAccount() throws Exception {
+        log("Deleting account");
+        more_button().click();
+        try {
+            action.press(followers()).moveTo(more_tab_nav_bar()).release().perform();
+            log("Used 'followers'");
+        } catch (Exception ignored) {
+            try {
+                action.press(more_tab_build_a_following()).moveTo(more_tab_nav_bar()).release().perform();
+                log("Used 'build a following'");
+            }catch (Exception e) {
+                throw new InterruptedException("Both failed");
+            }
+        }
+        account_settings().click();
+        delete_account().click();
+        confirm_delete().click();
+        confirm_delete_again().click();
+        log("Account deleted");
+
+    }
+
 }
