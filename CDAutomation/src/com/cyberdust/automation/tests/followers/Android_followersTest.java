3eb46dd9bd0ee20006cdea26f6f9918c0f68227c
BrantK <brant@cyberdust.com>
2016-05-04 18:31:07 -0700

    Fixed tests

--- CDAutomation/src/com/cyberdust/automation/tests/followers/Android_followersTest.java
+++ CDAutomation/src/com/cyberdust/automation/tests/followers/Android_followersTest.java
@@ -1,9 +1,5 @@
 package com.cyberdust.automation.tests.followers;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import com.cyberdust.automation.elements.AndroidElements;
 import com.cyberdust.automation.elements.LoginWith;
 
@@ -11,58 +7,56 @@ public class Android_followersTest extends AndroidElements {
 	LoginWith loginAs = new LoginWith();
 	
 	public void test01_add_friends() throws Exception {
-		
 		loginAs.user(followers_account01, followers_password01);
+
 		more_button().click();
+        Thread.sleep(1000);
+        driver.swipe(screenWidth/2, screenHeight/2, screenWidth/2, screenHeight/3, 200);
 		followers().click();
 		add_friend().click();
 		back_button().click();
 		friends().click();
+
 		try {
-			WebElement friend = name(followers_account02);
 			log("Friend added from followers menu ");
-			action().longPress(friend, 3000).release().perform();
+			action().longPress(name(followers_account02), 3000).release().perform();
 			unfollow_button().click();
 			okay_button().click();
 		}
-		catch (Exception e)
-		{
+		catch (Exception e) {
 			log("[Error] Unable to add friend from followers menu");
 		}
 
 		back_button().click();
-
 	}
 	
 	public void test02_add_friends() throws Exception {
-		
 
 		followers().click();
-		WebElement first_friend = name(followers_account02);
-		action().longPress(first_friend, 3000).release().perform();
+		action().longPress(name(followers_account02), 3000).release().perform();
 		
 		blast_more_block().click();
 		okay_button().click();
 		Thread.sleep(1000);
 
 		aDriver().pressKeyCode(4);
-
 		back_button().click();
 
-
-		action().press(followers()).moveTo(back_button()).release().perform();
+        Thread.sleep(1000);
+        driver.swipe(screenWidth / 2, screenHeight - 20, screenWidth / 2, 20, 300);
 		muted_blocked_users().click();
 		log("clicked muted users");
 		Thread.sleep(5000);
+
 		try {
 			if (name(followers_account02).isDisplayed())
 				log("Follower can be blocked");
 		} catch (Exception e) {
 			log("[Error] Couldn't block the followers");
 		}
+
 		name(followers_account02).click();
 		back_button().click();
 		back_button().click();
 	}
-	
 }
